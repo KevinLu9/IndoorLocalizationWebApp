09acee5eedd4881e4d811ccfb3cd776be97ee792
kevin.lu902 <kevin.lu902@gmail.com>
2023-04-18 02:12:04 +1000

    Added sorting of Bluetooth data by using a dictionary
    Display of Bluetooth rssi values was improved

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-var bluetooth_data_dict = {};
+class BluetoothDeviceData {
+  constructor(name, time, rssi, txPower){
+    this.name = name;
+    this.time = [time];
+    this.rssi = [rssi];
+    this.txPower = txPower;
+  }
+
+  addTime(time) {
+    this.time.push(time);
+  }
+
+  addRSSI(rssi) {
+    this.rssi.push(rssi);
+  }
+
+  setTxPower(txPower) {
+    this.txPower = txPower;
+  }
+}
+
+var bluetooth_data_dict = {'TestDevice': new BluetoothDeviceData('TestDevice', new Date().getTime(), 60, 30)};
 
 async function onButtonClick() {
   let filters = [];
   let options = {};
   options.acceptAllAdvertisements = true;
   //options.filters = filters;
-
+  bluetooth_data_dict = {}; // Reset the bluetooth data dictionary every button press
+  document.getElementById('scan-button').innerHTML = "Restart Scan";
   try {
     let scanText = document.getElementById("BLEScanList");
     if (!navigator.bluetooth.getAvailability()) {
@@ -32,53 +54,71 @@ async function onButtonClick() {
       JSON.stringify(scan.filters) +
       "</li>";
 
-      const logDataView = (labelOfDataSource, key, valueDataView) => {
-        const hexString = [...new Uint8Array(valueDataView.buffer)]
-          .map((b) => {
-            return b.toString(16).padStart(2, "0");
-          })
-          .join(" ");
-        const textDecoder = new TextDecoder("ascii");
-        const asciiString = textDecoder.decode(valueDataView.buffer);
-        return (
-          `  ${labelOfDataSource} Data: ` +
-            key +
-            "<br>    (Hex) " +
-            hexString +
-            "<br>    (ASCII) " +
-            asciiString
-        );
-      };
+    const logDataView = (labelOfDataSource, key, valueDataView) => {
+      const hexString = [...new Uint8Array(valueDataView.buffer)]
+        .map((b) => {
+          return b.toString(16).padStart(2, "0");
+        })
+        .join(" ");
+      const textDecoder = new TextDecoder("ascii");
+      const asciiString = textDecoder.decode(valueDataView.buffer);
+      return (
+        `  ${labelOfDataSource} Data: ` +
+        key +
+        "<br>    (Hex) " +
+        hexString +
+        "<br>    (ASCII) " +
+        asciiString
+      );
+    };
+
+    const getCurrentTime = (ms_time) => {
+      time = new Date();
+      time.setTime(ms_time);
+      return time.getDay() +
+          "/" +
+          time.getMonth() +
+          "/" +
+          time.getFullYear() +
+          " | " +
+          (time.getHours() % 12) +
+          ":" +
+          time.getMinutes() +
+          ":" +
+          time.getSeconds() +
+          "." +
+          time.getMilliseconds() +
+          (time.getHours() < 12 ? " AM" : " PM");
+    }
 
     navigator.bluetooth.addEventListener("advertisementreceived", (event) => {
       if (event.device.name != null) {
-        time = new Date();
-        let scanTextString = "<li class='list-group-item'>" +
-          "<br>Device Name: " +
-          event.device.name +
-          "<br>Time Captured: " +
-          time.getDay() + "-" + time.getMonth() + "-" + time.getYear() + " " + 
-          time.getHours()%12 + ":" + time.getMinutes() + ":" + time.getSeconds() + "." + 
-          time.getMilliseconds() + ((time.getHours() < 12) ? " AM" : " PM") +
-          "<br>RSSI: " +
-          event.rssi +
-          "<br>TX Power: " +
-          event.txPower +
-          "<br>UUIDs: " +
-          event.uuids + "<br>";
-          event.manufacturerData.forEach((valueDataView, key) => {
-            scanTextString += logDataView("Manufacturer", key, valueDataView);
-          })
+        if (bluetooth_data_dict[event.device.name] != undefined) {
+          // Add data to existing device in dictionary
+          bluetooth_data_dict[event.device.name].addTime(new Date().getTime());
+          bluetooth_data_dict[event.device.name].addRSSI(event.rssi);
+          bluetooth_data_dict[event.device.name].setTxPower(event.txPower);
 
-          //scanTextString += "<br> Service Data: "
+          // Update new device to website display
+          let deviceDisplay = document.getElementById(event.device.name);
+          deviceDisplay.innerHTML = `
+            name: ${event.device.name}, <br/>
+            Last Packet Time: ${getCurrentTime(bluetooth_data_dict[event.device.name].time[bluetooth_data_dict[event.device.name].time.length-1])}, <br/>
+            rssi: ${bluetooth_data_dict[event.device.name].rssi},<br/>
+            txPower: ${bluetooth_data_dict[event.device.name].txPower} <br/>`;
+        }
+        else {
+          // Add new device to dictionary
+          bluetooth_data_dict[event.device.name] = new BluetoothDeviceData(event.device.name, new Date().getTime(), event.rssi, event.txPower);
 
-          event.serviceData.forEach((valueDataView, key) => {
-            scanTextString += logDataView("Service", key, valueDataView);
-          });
-           
-        scanTextString += "</li>";
-        scanText.innerHTML += scanTextString;
-
+          // Add new device to website display
+          scanText.innerHTML += `<li class='list-group-item' id='${event.device.name}' style='word-wrap: break-word'>
+            name: ${event.device.name}, <br/>
+            time: ${getCurrentTime(bluetooth_data_dict[event.device.name].time[0])}, <br/>
+            rssi: ${bluetooth_data_dict[event.device.name].rssi},<br/>
+            txPower: ${bluetooth_data_dict[event.device.name].txPower} <br/> 
+          </li>`;
+        }
       }
     });
 
@@ -92,9 +132,10 @@ async function onButtonClick() {
         scan.active +
         "</li>";
     }
+    //setTimeout(stopScan, 10000);
   } catch (error) {
     console.log("Argh! " + error);
   }
 }
 
-setTimeout(stopScan, 10000);
+
