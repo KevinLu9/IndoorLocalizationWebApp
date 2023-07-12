c23c425778bf5bd0e2e6350830c4a021ff0dbc94
KevinLu9 <kevin.lu902@gmail.com>
2023-07-12 23:20:18 +1000

    removed example routes from svelte app

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -6,12 +6,4 @@
 </script>
 
 
-<h1 class="font-bold">Routify Starter Template</h1>
-
-<p>
-    To see an example app that shows off a lot of Routify's features, go to <a href="/example">/example</a>
-</p>
-
-<p>
-    This template is ready to be used in production! just delete the example app at: src/pages/example
-</p>
+<h1 class="font-bold">Home Page</h1>
