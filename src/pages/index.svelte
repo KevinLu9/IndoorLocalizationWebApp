c56b6a15a0921d2647a56b23b20a98a6228aa593
KevinLu9 <kevin.lu902@gmail.com>
2023-09-19 04:43:21 +1000

    fixed ui typos

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -23,7 +23,7 @@
 </script>
 
 
-<h1 class="font-bold text-center text-2xl">Projects Nearby</h1>
+<h1 class="font-bold text-center text-2xl my-2">Projects Nearby</h1>
 {#if beaconToShow}
     <p class="text-center">
         Project ID: {beaconToShow.id}
@@ -33,6 +33,6 @@
     </div>
 {:else}
     <div class="m-4 p-4 bg-200-600 rounded-lg border border-black no-tailwindcss-base">
-        <h3 class="text-center text-red-600">You are not near any displays!</h3>
+        <h3 class="text-center text-red-600">You are no nearby displays!</h3>
     </div>
 {/if}
