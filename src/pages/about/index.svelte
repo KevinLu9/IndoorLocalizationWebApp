88642944a2ef06ccc8c5ba7321f40706c4f0e8fc
KevinLu9 <kevin.lu902@gmail.com>
2023-10-22 18:46:44 +1100

    updated about us page

--- src/pages/about/index.svelte
+++ src/pages/about/index.svelte
@@ -1,1 +1,47 @@
-<p>About Page</p>
+<div class="w-full flex flex-col p-4 items-center justify-center">
+  <p class="text-2xl font-bold text-center">About Us</p>
+  <div class="bg-white rounded-md outline outline-black p-5 my-2">
+    <img class="max-h-[100px]" src="images/monash_logo.png" alt="Monash University Logo" />
+  </div>
+  
+
+  <ul class="w-full flex flex-col gap-4 justify-between pb-4">
+    <li>
+      <div class="flex flex-col items-center text-center">
+        <p class="font-bold italic">Student/Developer</p>
+        <p>
+          Kevin Lu
+        </p>
+      </div>
+    </li>
+    <li>
+      <div class="flex flex-col items-center text-center">
+        <p class="font-bold italic">Supervisor</p>
+        <p>
+          Dr Mehmet Yuce
+        </p>
+      </div>
+    </li>
+    <li>
+      <div class="flex flex-col items-center text-center">
+        <p class="font-bold italic">Special Thanks To</p>
+        <p>
+          Maggie Gendy
+        </p>
+      </div>
+      
+    </li>
+    
+    
+  </ul>
+  
+  <p>
+    This application was created for a Final Year Engineering Project at Monash University.
+    The FYP was hosted by the Electrical and Computer Systems Engineering (ECSE) department.
+    The aim of the project was to create an interactive and engaging platform that allows users to see informative content based on their proximity to certain locations.<br/> <br/>
+    This project addresses the challenge of accurate indoor localization for targeted advertising without requiring users to carry additional dedicated hardware. 
+    It leverages the availability of smartphones and the capabilities they offer, making the application accessible through web browsers rather than requiring native app installations. 
+    This approach aligns well with the current reliance on smartphones as much of the population own these devices.
+  </p>
+
+</div>
